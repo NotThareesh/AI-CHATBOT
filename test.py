fdf7d9d113703a84bb5e909b511beaeb93f8c3cd
NotThareesh <thareesh.prabakaran@gmail.com>
2023-09-07 14:29:31 +0400

    Optimizations to Model

--- test.py
+++ test.py
@@ -5,52 +5,46 @@ import torch
 
 start1 = time.time()
 
-model = SentenceTransformer("all-MiniLM-L6-v2")
-
-file = open('data.csv', mode='r', newline='\n')
-reader = csv.reader(file, delimiter=',')
-next(reader)
-
-prompt = "Harms of microorganisms"
-
-embeddings1 = model.encode(prompt, convert_to_tensor=True)
-
 
 def data_generator():
+    file = open('data.csv', mode='r', newline='\n')
+    reader = csv.reader(file, delimiter=',')
+    next(reader)
+
     for contents in reader:
         yield tuple(contents)
 
+    file.close()
 
-def find_similarity():
+
+def find_similarity(prompt):
+    model = SentenceTransformer("all-MiniLM-L6-v2")
+
+    embeddings1 = model.encode(prompt, convert_to_tensor=True)
+
     for data in tuple(data_generator()):
-        count = 0
         total_similarity = 0
 
-        content = data[1].split('\t')
+        embeddings2 = model.encode(data[1], convert_to_tensor=True)
+        similarity = util.pytorch_cos_sim(embeddings1, embeddings2)
+        total_similarity += similarity
 
-        for sentence in content:
-            count += 1
-            embeddings2 = model.encode(sentence, convert_to_tensor=True)
-            similarity = util.pytorch_cos_sim(embeddings1, embeddings2)
-            total_similarity += similarity
+        # If data matches very closely in the first go then return
+        if torch.gt(total_similarity, torch.tensor([[0.35]])):
+            return data[2]
 
-            if count >= 10 and torch.gt(total_similarity/count, torch.tensor([[0.40]])):
-                break
-
-            if torch.gt(total_similarity/count, torch.tensor([[0.50]])):
-                return ("Data Matched", data[2])
-
-            if torch.lt(total_similarity/count, torch.tensor([[0.0]])):
-                break
+        # If average similarity ever goes -ve, then break
+        if torch.lt(total_similarity, torch.tensor([[0.05]])):
+            continue
 
     return "No Data Matched"
 
 
 end1 = time.time()
 
-# Time for running function
+# Time taken for finding similarity
 start2 = time.time()
-print(find_similarity())
+print(find_similarity("Water is important"))
 end2 = time.time()
 
 print("To read data", end1 - start1)
